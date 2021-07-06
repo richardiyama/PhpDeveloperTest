b0878ecfb3ad9ac5d47eb469bee88e1b4f44c969
Richard Iyama <richardiyama@yahoo.com>
2021-07-06 17:46:13 +0100

    new commits

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@
 
 Route::group(['prefix'  =>  'admin'], function () {
 
-Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+Route::get('login', 'App\Http\Controllers\Admin\LoginController@showLoginForm')->name('admin.login');
 Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
 Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
 
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 require 'admin.php';
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
